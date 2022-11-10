39c725a55aaf8a7ce4cbd234a1e62db6d1d2bb64
Qboi123 <qboiwastaken@gmail.com>
2022-11-10 10:29:09 +0100

    Updat

--- src/main/java/com/ultreon/mods/pixelguns/model/InfinityGunRenderer.java
+++ src/main/java/com/ultreon/mods/pixelguns/model/InfinityGunRenderer.java
@@ -1,7 +1,6 @@
 package com.ultreon.mods.pixelguns.model;
 
 import com.ultreon.mods.pixelguns.item.InfinityGunItem;
-import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoItemRenderer;
 
 public class InfinityGunRenderer extends GeoItemRenderer<InfinityGunItem> {
