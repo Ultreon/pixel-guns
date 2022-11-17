27bb52f4b1806c3d19567ee215dedf75f8050ffe
Skylandia <Skylandia>
2022-11-18 00:20:25 +1300

    Add particles when bullet collides with block

--- src/main/java/com/ultreon/mods/pixelguns/entity/projectile/Bullet.java
+++ src/main/java/com/ultreon/mods/pixelguns/entity/projectile/Bullet.java
@@ -2,6 +2,8 @@ package com.ultreon.mods.pixelguns.entity.projectile;
 
 import com.ultreon.mods.pixelguns.entity.ModEntities;
 
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.damagesource.DamageSource;
@@ -14,9 +16,11 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+
 import org.jetbrains.annotations.NotNull;
 
 public class Bullet extends AbstractBulletEntity {
@@ -54,6 +58,7 @@ public class Bullet extends AbstractBulletEntity {
         }
     }
 
+    @Override
     protected void onHitEntity(@NotNull EntityHitResult entityHitResult) {
         super.onHitEntity(entityHitResult);
         if (this.level.isClientSide || entityHitResult.getEntity() instanceof WitherBoss && ((WitherBoss) entityHitResult.getEntity()).isPowered()) {
@@ -65,6 +70,14 @@ public class Bullet extends AbstractBulletEntity {
         entity.invulnerableTime = 0;
     }
 
+    @Override
+    protected void onHitBlock(BlockHitResult blockHitResult) {
+        
+        this.level.addParticle(new BlockParticleOption(ParticleTypes.BLOCK, this.level.getBlockState(blockHitResult.getBlockPos())), this.getX() + (this.random.nextDouble() - 0.5), this.getY() + 0.1, this.getZ() + (this.random.nextDouble() - 0.5), 0, 1.5, 0);
+        super.onHitBlock(blockHitResult);
+    }
+
+    @Override
     protected void onHit(@NotNull HitResult hitResult) {
         super.onHit(hitResult);
         if (!this.level.isClientSide()) {
