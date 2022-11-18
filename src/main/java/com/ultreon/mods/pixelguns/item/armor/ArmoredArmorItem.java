521a0c9baedb62704d2b1ce4e3208db030836e8f
Skylandia <Skylandia>
2022-11-18 14:09:57 +1300

    Move Armor Related Items to Armor Package

--- src/main/java/com/ultreon/mods/pixelguns/item/armor/ArmoredArmorItem.java
+++ src/main/java/com/ultreon/mods/pixelguns/item/armor/ArmoredArmorItem.java
@@ -1,9 +1,8 @@
-package com.ultreon.mods.pixelguns.item;
+package com.ultreon.mods.pixelguns.item.armor;
 
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.Item;
 
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.manager.AnimationData;
@@ -13,13 +12,12 @@ public class ArmoredArmorItem extends ArmorItem implements IAnimatable {
 	
 	private AnimationFactory factory = new AnimationFactory(this);
 
-	public ArmoredArmorItem(ArmorMaterial materialIn, EquipmentSlot slot, Item.Properties builder) {
+	public ArmoredArmorItem(ArmorMaterial materialIn, EquipmentSlot slot, Properties builder) {
 		super(materialIn, slot, builder);
 	}
 
 	@Override
-	public void registerControllers(AnimationData data) {
-	}
+	public void registerControllers(AnimationData data) {}
 
 	@Override
 	public AnimationFactory getFactory() {
