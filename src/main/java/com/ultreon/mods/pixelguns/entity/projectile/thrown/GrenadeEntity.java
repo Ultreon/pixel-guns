fe541aba0f31b863e3561aab396a386b0a7b2495
Skylandia <Skylandia>
2022-11-21 17:12:51 +1300

    Play Custom Explosion On Collision

--- src/main/java/com/ultreon/mods/pixelguns/entity/projectile/thrown/GrenadeEntity.java
+++ src/main/java/com/ultreon/mods/pixelguns/entity/projectile/thrown/GrenadeEntity.java
@@ -2,15 +2,20 @@ package com.ultreon.mods.pixelguns.entity.projectile.thrown;
 
 import com.ultreon.mods.pixelguns.entity.ModEntities;
 import com.ultreon.mods.pixelguns.item.ModItems;
+import com.ultreon.mods.pixelguns.sound.ModSounds;
+
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion.DestructionType;
+
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -52,6 +57,7 @@ public class GrenadeEntity extends ThrownItemEntity implements IAnimatable {
     protected void onCollision(HitResult hitResult) {
         super.onCollision(hitResult);
         explode();
+        world.playSound(hitResult.getPos().x, hitResult.getPos().y, hitResult.getPos().z, ModSounds.GRENADE_EXPLODE, SoundCategory.MASTER, 0.8f, 0.8f, false);
     }
 
     @Override
