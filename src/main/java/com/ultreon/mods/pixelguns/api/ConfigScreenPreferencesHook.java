7db6bc265c7a467f9691fe567d5581466f3fd9df
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-12-17 12:04:34 +1100

    Update ConfigScreenPreferencesHook.java

--- src/main/java/com/ultreon/mods/pixelguns/api/ConfigScreenPreferencesHook.java
+++ src/main/java/com/ultreon/mods/pixelguns/api/ConfigScreenPreferencesHook.java
@@ -1,14 +1,22 @@
 package com.ultreon.mods.pixelguns.api;
 
+import static com.ultreon.mods.pixelguns.api.ConfigScreenPreferencesHook.Companion.disabledCustomConfigScreen;
+
 /**
  * Run with {@code FabricLoader.getInstance().getEntrypoints("pixelguns:configScreenPreferencesHook", ConfigScreenPreferencesHook.class).forEach(ConfigScreenPreferencesHook::onConfigPreferencesInit);}
  */
 @FunctionalInterface
 public interface ConfigScreenPreferencesHook {
-    public static boolean disabledCustomConfigScreen = false;
+    static class Companion {
+        static boolean disabledCustomConfigScreen = false;
+    }
     public void onConfigPreferencesInit();
 
     public static boolean customConfigScreenDisabled() {
         return disabledCustomConfigScreen;
+    }
+
+    public static void disableCustomConfigScren() {
+        disabledCustomConfigScreen = true;
     }
 }
