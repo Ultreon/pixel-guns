71494b3a5890c8acaf59fccebee7e7086bca8788
Skylandia <xrealify@gmail.com>
2022-12-28 01:38:53 +1300

    Remove easter egg audio

--- src/main/java/com/ultreon/mods/pixelguns/item/gun/variant/HeavyAssaultRifleItem.java
+++ src/main/java/com/ultreon/mods/pixelguns/item/gun/variant/HeavyAssaultRifleItem.java
@@ -1,20 +1,10 @@
 package com.ultreon.mods.pixelguns.item.gun.variant;
 
-import com.ultreon.mods.pixelguns.PixelGuns;
 import com.ultreon.mods.pixelguns.registry.ModItems;
 import com.ultreon.mods.pixelguns.item.gun.GunItem;
 import com.ultreon.mods.pixelguns.sound.ModSounds;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.NotNull;
 
 public class HeavyAssaultRifleItem extends GunItem {
-    private final SoundEvent shootSoundEasterEgg;
 
     public HeavyAssaultRifleItem(Settings settings) {
         super(
@@ -39,24 +29,5 @@ public class HeavyAssaultRifleItem extends GunItem {
                 22,
                 40
         );
-        this.shootSoundEasterEgg =  ModSounds.ASSAULTRIFLE_HEAVY_EG;
-    }
-
-
-    @Override
-    public void playShootSound(World world, PlayerEntity user, ItemStack stack) {
-        NbtCompound nbtElement = stack.getOrCreateSubNbt(PixelGuns.NBT_NAME);
-        boolean easterEgg;
-        if (!nbtElement.contains("easterEgg", NbtElement.BYTE_TYPE))
-            nbtElement.putBoolean("easterEgg", (easterEgg = user.getRandom().nextInt(100) == 0));
-        else
-            easterEgg = nbtElement.getBoolean("easterEgg");
-
-        world.playSound(null, user.getX(), user.getY(), user.getZ(), easterEgg ? this.shootSoundEasterEgg : this.shootSound, SoundCategory.MASTER, 1.0f, 1.0f);
-    }
-
-    @Override
-    public void onCraft(@NotNull ItemStack itemStack, @NotNull World level, @NotNull PlayerEntity player) {
-        super.onCraft(itemStack, level, player);
     }
 }
