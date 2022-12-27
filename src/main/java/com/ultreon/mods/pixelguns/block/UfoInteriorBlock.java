791ab11c5ab93eb57a452c180828299af4743d38
Skylandia <xrealify@gmail.com>
2022-12-27 18:37:42 +1300

    Use directional outline shapes

--- src/main/java/com/ultreon/mods/pixelguns/block/UfoInteriorBlock.java
+++ src/main/java/com/ultreon/mods/pixelguns/block/UfoInteriorBlock.java
@@ -1,12 +1,7 @@
 package com.ultreon.mods.pixelguns.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
-import net.minecraft.world.BlockView;
 
 public class UfoInteriorBlock extends EasyBlock {
     public UfoInteriorBlock(Settings settings) {
@@ -24,7 +19,7 @@ public class UfoInteriorBlock extends EasyBlock {
     }
 
     @Override
-    public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPos, ShapeContext shapeContext) {
-        return VoxelShapes.cuboid(-0.4f, 0.0f, -0.4f, 0.5f, 0.3f, 1.4f);
+    public VoxelShape getShape() {
+        return VoxelShapes.cuboid(0.0f, 0.0f, 0.2f, 1.0f, 0.3f, 1.0f);
     }
 }
