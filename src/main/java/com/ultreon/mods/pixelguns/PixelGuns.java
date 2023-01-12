5c238a73e6dc93a2a805d891b0599e3e82ef3070
Skylandia <xrealify@gmail.com>
2023-01-13 00:25:51 +1300

    Make server aware of registered items/sounds/entities/blocks

--- src/main/java/com/ultreon/mods/pixelguns/PixelGuns.java
+++ src/main/java/com/ultreon/mods/pixelguns/PixelGuns.java
@@ -2,6 +2,10 @@ package com.ultreon.mods.pixelguns;
 
 import com.ultreon.mods.pixelguns.item.gun.GunItem;
 
+import com.ultreon.mods.pixelguns.registry.BlockRegistry;
+import com.ultreon.mods.pixelguns.registry.EntityRegistry;
+import com.ultreon.mods.pixelguns.registry.ItemRegistry;
+import com.ultreon.mods.pixelguns.registry.SoundRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.util.Hand;
@@ -22,6 +26,12 @@ public class PixelGuns implements ModInitializer {
     }
 
     public void onInitialize() {
+        new EntityRegistry();
+        new ItemRegistry();
+        new BlockRegistry();
+        new SoundRegistry();
+
+
         ServerPlayNetworking.registerGlobalReceiver(new Identifier(MOD_ID, "reload"), (server, player, serverPlayNetworkHandler, buf, packetSender) -> {
             if (player.getStackInHand(Hand.MAIN_HAND).getItem() instanceof GunItem) {
                 player.getStackInHand(Hand.MAIN_HAND).getOrCreateNbt().putBoolean("isReloading", buf.readBoolean());
